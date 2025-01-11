b317fb8f91a3f024346a136b97b0af2efc9873fe
+ <+>
2025-01-11 16:11:23 +0300

    adds

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main=document.querySelector('main').remove();
+const newHeader=document.createElement('h1');
+newHeader.id='victory'
+newHeader.innerHTML=`<h1 id="victory">Jimmy is the champion</h1>`;
+console.log(newHeader);
